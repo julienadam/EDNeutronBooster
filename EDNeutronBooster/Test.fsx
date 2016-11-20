abaac67091964bade47b6cbe6ae23e5f2bbe1fd6
Julien Adam <julien.adam@gmail.com>
2016-11-20 11:58:24 +0100

    Added neutron stars that aren't the main star but are close it

--- EDNeutronBooster/Test.fsx
+++ EDNeutronBooster/Test.fsx
@@ -6,26 +6,35 @@ open FSharp.Data.Runtime.BaseTypes
 open FSharp.Data.CsvExtensions
 open System.Collections.Generic
 
-type Point3D = { X:decimal; Y:decimal; Z:decimal}
+type Point3D = { X:decimal; Y:decimal; Z:decimal }
 type SystemInfo = { Location : Point3D; Id: int; Name: string }
 
-let mid sys1 sys2 = { X = (sys2.X - sys1.X) / 2m; Y = (sys2.Y - sys1.Y) / 2m; Z = (sys2.Z - sys1.Z) / 2m } 
+let mid sys1 sys2 = { X = (sys2.X - sys1.X) / 2m; Y = (sys2.Y - sys1.Y) / 2m; Z = (sys2.Z - sys1.Z) / 2m }
 let sqrt (x:decimal) = (decimal) (System.Math.Sqrt((float) x))
-let squared (x:decimal) =(decimal) (System.Math.Pow((float) x, 2.))
+let squared (x:decimal) = (decimal) (System.Math.Pow((float) x, 2.))
 let dist sys1 sys2 = sqrt (squared (sys2.X - sys1.X) + squared(sys2.Y - sys1.Y) + squared (sys2.Z - sys1.Z))
 
 type BodyDescription = JsonProvider< @"{""id"":1497,""created_at"":1466612947,""updated_at"":1477503588,""name"":""42 AQUILAE A"",""system_id"":205,""group_id"":2,""group_name"":""Star"",""type_id"":null,""type_name"":null,""distance_to_arrival"":null,""full_spectral_class"":null,""spectral_class"":""F"",""spectral_sub_class"":null,""luminosity_class"":null,""luminosity_sub_class"":null,""surface_temperature"":7350,""is_main_star"":null,""age"":1720,""solar_masses"":1.2578,""solar_radius"":1.2509,""catalogue_gliese_id"":null,""catalogue_hipp_id"":""95556"",""catalogue_hd_id"":""185124"",""volcanism_type_id"":null,""volcanism_type_name"":null,""atmosphere_type_id"":null,""atmosphere_type_name"":null,""terraforming_state_id"":null,""terraforming_state_name"":null,""earth_masses"":null,""radius"":null,""gravity"":null,""surface_pressure"":null,""orbital_period"":296444.7,""semi_major_axis"":43.13,""orbital_eccentricity"":0.3455,""orbital_inclination"":39.4,""arg_of_periapsis"":104.98,""rotational_period"":0,""is_rotational_period_tidally_locked"":false,""axis_tilt"":0,""eg_id"":1808,""belt_moon_masses"":null,""rings"":[],""atmosphere_composition"":[],""solid_composition"":[],""materials"":[],""is_landable"":0}">
+
+let maxDistanceFromMainStar = 100m
 
 let neutronSystems = 
     System.IO.File.ReadLines(@"c:\users\nidhogg\desktop\bodies.jsonl") 
     |> Seq.choose (fun l -> 
         let parsed = BodyDescription.Parse l
         match parsed.TypeName.JsonValue with
-        | JsonValue.String("Neutron star") -> Some(parsed.Id)
-        | _ -> None
-        )
-    |> Set.ofSeq
+        | JsonValue.String("Neutron star") -> 
+            if parsed.IsMainStar.JsonValue = JsonValue.Boolean(true) then
+                Some(parsed.Id, 0m)
+            else
+                match parsed.DistanceToArrival.JsonValue with
+                | JsonValue.Number d -> 
+                    if d > 0m && d <= maxDistanceFromMainStar then Some(parsed.Id, d) else None
+                | _ -> None
+        | _ -> None)
+    |> Map.ofSeq
 
+neutronSystems |> Seq.choose (fun kvp -> if kvp.Value > 0m then Some(kvp.Key,kvp.Value) else None) |> List.ofSeq
 
 let neutronSystemDetails =
     System.IO.File.ReadLines(@"c:\users\nidhogg\desktop\systems.csv")
@@ -47,7 +56,7 @@ let neutronSystemDetails =
         with
             | _ -> None
         )  
-    |> Seq.filter (fun sys -> neutronSystems |> Set.contains sys.Id)
+    |> Seq.filter (fun sys -> neutronSystems |> Map.containsKey sys.Id)
     |> List.ofSeq 
 let sol = { Location = { X = 0m; Y = 0m; Z = 0m }; Id = -11; Name = "Sol" }
 let CG = { Location = { X = -5805.78125m; Y = 129.875m; Z = -5969.875m }; Id = -22; Name = "CG location" }
@@ -61,7 +70,7 @@ let systemsInSphere =
     |> Seq.filter (fun sys -> dist midpoint sys.Location < radius)
     |> List.ofSeq
 
-// Add the star and end points
+// Add the start and goal points
 let allSystemsConsidered = [sol; CG] @ systemsInSphere
 
 let range = 56m * 4.0m
